0620e748d2a4baae7a659e460bfa18952c5a37c1
shraddha1998 <32254132+shraddha1998@users.noreply.github.com>
2019-07-17 07:42:47 +0530

    Add files via upload

---
+++ swapusingbitwiseop.c
@@ -0,0 +1,17 @@
+//Write a program to swap two numbers using bitwise operators.
+
+#include<stdio.h>
+int main()
+{
+int a, b,temp;
+printf("Enter two numbers ");
+scanf("%d %d",&a,&b);
+printf("Before swapping a=%d and b=%d\n",a,b);
+a=a^b;
+b=a^b;
+a=a^b;
+printf("After swapping a=%d and b=%d\n",a,b);
+return 0;
+
+}
+
